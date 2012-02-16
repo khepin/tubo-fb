98b5dcc25f1136d8c4a90724864d64ede390a4b8
Seb Armand <khepin@gmail.com>
2012-02-16 14:54:47 +0800

    command can be used

--- src/Khepin/TuboBundle/Command/UploadNewCommand.php
+++ src/Khepin/TuboBundle/Command/UploadNewCommand.php
@@ -21,29 +21,63 @@ class UploadNewCommand extends ContainerAwareCommand {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+        $q = $em->createQuery('SELECT MAX(p.tubo_id) FROM KhepinTuboBundle:Photo p');
+        $last_known_id = $q->getSingleScalarResult();
+        $pics = $this->getRecentPics($last_known_id, $em);
+        $output->writeln('Found <info>' . count($pics) . '</info> new pictures.');
+
         $user = $this->getContainer()->get('doctrine')->getRepository('KhepinTuboBundle:User')
                 ->findOneBy(array('username' => '621327693'));
+
+        foreach ($pics as $pic) {
+            $output->writeln('Uploading picture: <info>'.$pic->getLegend().'</info>');
+            $this->uploadPic($pic, $user);
+        }
+    }
+
+    protected function uploadPic(Photo $photo, $user) {
         $buzz = $this->getContainer()->get('buzz');
         $access_token = $user->getAccessToken();
-        $graph_url= 'https://graph.facebook.com';
-        $resource = '/'.$user->getUsername().'/photos?access_token=' .$access_token;
-        echo $access_token;
+        $domain = 'https://graph.facebook.com';
+        $resource = '/' . $user->getUsername() . '/photos?access_token=' . $access_token;
         $buzz->getClient()->setTimeout(20000);
-        $file = $buzz->get('http://p.mytubo.net/Pictures/Original/2012-02-15/1329323372722_155287.jpg')->getContent();
+        $file = $buzz->get($photo->getLink())->getContent();
 
         $form = new FormUpload();
         $form->setContent($file);
         $form->setFilename('picture.jpg');
         $form->setName('source');
-        
-        $request = new FormRequest('POST', $resource, $graph_url);
-        $request->setField('name', 'Some Name');
+
+        $request = new FormRequest('POST', $resource, $domain);
+        $request->setField('name', $photo->getLegend());
         $request->setField('source', $form);
-        
+
         $response = new \Buzz\Message\Response();
         $buzz->getClient()->send($request, $response);
-        
-        var_dump($response);
+    }
+
+    protected function getRecentPics($last_known_id, $em = null) {
+        $buzz = $this->getContainer()->get('buzz');
+        $response = $buzz->get(
+                'http://b.mytubo.net/index.php?action=getfeedsNew&TargetUserID=155287&Limit=10&Reflesh=0&MinPicID=99999999&UserID=-1&Email=&Token=ff4009e330710727c594c7698e8a0b2a&_=1329303139384
+        ');
+        $data = json_decode($response->getContent());
+        $new = array();
+        foreach ($data->data as $pic) {
+            if ($pic->PicID > $last_known_id) {
+                $photo = new Photo();
+                $photo->setTuboId($pic->PicID);
+                $photo->setLegend($pic->Description);
+                $photo->setLink($pic->OriURL);
+                if (!is_null($em)) {
+                    $em->persist($photo);
+                }
+                $new[] = $photo;
+            }
+        }
+        $em->flush();
+        return $new;
     }
 
 }
--- src/Khepin/TuboBundle/Controller/DefaultController.php
+++ src/Khepin/TuboBundle/Controller/DefaultController.php
@@ -18,48 +18,25 @@ class DefaultController extends Controller {
      */
     public function indexAction() {
         $buzz = $this->get('buzz');
-//        $response = $buzz->get('http://b.mytubo.net/index.php?action=getfeedsNew&TargetUserID=155287&Limit=10&Reflesh=0&MinPicID=99999999&UserID=-1&Email=&Token=ff4009e330710727c594c7698e8a0b2a&_=1329303139384');
-//        $name = json_decode($response->getContent());
-//
-//        $em = $this->getDoctrine()->getEntityManager();
-//        $q = $em->createQuery('SELECT MAX(p.tubo_id) FROM KhepinTuboBundle:Photo p');
-//        $max = $q->getSingleScalarResult();
-//        echo $max;
-//        $new = array();
-//        foreach ($name->data as $pic) {
-//            if ($pic->PicID > $max) {
-//                $photo = new Photo();
-//                $photo->setTuboId($pic->PicID);
-//                $photo->setLegend($pic->Description);
-//                $photo->setLink($pic->OriURL);
-//                $em->persist($photo);
-//                $new[] = $photo;
-//            }
-//        }
-//        $em->flush();
-        
-        $access_token = $this->get('fos_facebook.api')->getAccessToken();
-        $graph_url= "https://graph.facebook.com";
-        $resource = "/621327693/photos?access_token=" .$access_token;
-//        $buzz->getClient()->setTimeout(20000);
-//        $file = $buzz->get('http://p.mytubo.net/Pictures/Original/2012-02-15/1329323372722_155287.jpg')->getContent();
-//
-//        $form = new FormUpload();
-//        $form->setContent($file);
-//        $form->setFilename('picture.jpg');
-//        $form->setName('source');
-//        
-//        $request = new FormRequest('POST', $resource, $graph_url);
-//        $request->setField('name', 'Some Name');
-//        $request->setField('source', $form);
-//        
-//        $response = new \Buzz\Message\Response();
-//        $buzz->getClient()->send($request, $response);
-//        var_dump($response);
-//        foreach($new as $photo){
-//            
-//        }
-        return array();
+        $response = $buzz->get('http://b.mytubo.net/index.php?action=getfeedsNew&TargetUserID=155287&Limit=10&Reflesh=0&MinPicID=99999999&UserID=-1&Email=&Token=ff4009e330710727c594c7698e8a0b2a&_=1329303139384');
+        $name = json_decode($response->getContent());
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $q = $em->createQuery('SELECT MAX(p.tubo_id) FROM KhepinTuboBundle:Photo p');
+        $max = $q->getSingleScalarResult();
+        echo $max;
+        $new = array();
+        foreach ($name->data as $pic) {
+            if ($pic->PicID > $max) {
+                $photo = new Photo();
+                $photo->setTuboId($pic->PicID);
+                $photo->setLegend($pic->Description);
+                $photo->setLink($pic->OriURL);
+                $em->persist($photo);
+                $new[] = $photo;
+            }
+        }
+        $em->flush();
     }
 
     public function importAction() {
